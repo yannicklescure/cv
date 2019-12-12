e4ff6de57375163a238fb077652a8d3c9a483d48
Yannick Lescure <yannick.lescure@gmail.com>
2019-12-11 19:06:38 -0500

    edits + add section portfolio

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-const faStars = document.querySelectorAll(".fa-square");
-faStars.forEach(faStar => {
-  faStar.classList.add("small");
-  faStar.classList.add("ml-1");
+console.log(`Thank you for visiting my code.
+If you read that message, visit https://github.com/yannicklescure/`);
+const faSquares = document.querySelectorAll(".fa-square");
+faSquares.forEach(faSquare => {
+  faSquare.classList.add("small");
+  faSquare.classList.add("ml-1");
 });
+
+const collapseBox = (anchor, box) => {
+  box.style.display = "none";
+  let openBox = false;
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+    // event.currentTarget.innerHTML = '';
+    if (openBox) {
+      event.currentTarget.innerHTML = '<i class="fas fa-chevron-right"></i>';
+      box.style.display = "none";
+      openBox = false;
+      // console.log('click false');
+    } else {
+      event.currentTarget.innerHTML = '<i class="fas fa-chevron-down"></i>';
+      box.style.display = "block";
+      openBox = true;
+      // console.log('click true');
+    }
+  });
+};
 
 const experienceAnchor = document.querySelector('a[href="#collapseExperience"]');
 const experienceBox = document.querySelector("#collapseExperience");
-experienceBox.style.display = "none";
-let openExperienceBox = false;
-experienceAnchor.addEventListener("click", (event) => {
-  event.preventDefault();
-  // event.currentTarget.innerHTML = '';
-  if (openExperienceBox) {
-    event.currentTarget.innerHTML = '<i class="fas fa-chevron-right"></i>';
-    experienceBox.style.display = "none";
-    openExperienceBox = false;
-    console.log('click false');
-  } else {
-    event.currentTarget.innerHTML = '<i class="fas fa-chevron-down"></i>';
-    experienceBox.style.display = "block";
-    openExperienceBox = true;
-    console.log('click true');
-  }
-});
+collapseBox(experienceAnchor, experienceBox);
+
+const portfolioAnchor = document.querySelector('a[href="#collapsePortfolio"]');
+const portfolioBox = document.querySelector("#collapsePortfolio");
+collapseBox(portfolioAnchor, portfolioBox);
